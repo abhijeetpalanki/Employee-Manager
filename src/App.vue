6b5fbe111c92b25d52346b17d9eac7101d9c70bd
abhijeetpalanki <ajpalanki@gmail.com>
2022-10-11 21:16:33 -0500

    basic ui setup and routes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Navbar />
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
-</script>
+import Navbar from "./components/Navbar.vue";
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default {
+  name: "App",
+  components: { Navbar }
+};
+</script>
