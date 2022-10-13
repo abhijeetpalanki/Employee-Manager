ce142e163ee5aa1502ea6ab5502a153bc30dfc85
abhijeetpalanki <ajpalanki@gmail.com>
2022-10-13 01:42:41 -0500

    crud operations for employee

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -2,5 +2,5 @@ import firebase from "firebase";
 import "firebase/firestore";
 import firebaseConfig from "./firebaseConfig";
 
-const firebaseApp = firebase.inititalizeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 export default firebaseApp.firestore();
